d03fc9e9a59b98c2feb99c5615c9bd0b45520184
wsw <30440419+xyz9836@users.noreply.github.com>
2020-09-18 20:35:59 +0800

    Create update_rules.py

---
+++ update_rules.py
@@ -0,0 +1,29 @@
+#coding=utf-8
+
+import requests
+
+def get_document(url):
+    try:
+        get = requests.get(url)
+        data = get.content
+        get.close()
+        return data
+    except:
+        return 0
+
+def download_file(url_x,file_name):
+    url_x=url_x+file_name
+    document = get_document(url_x)
+    if not document:
+        print('  {}下载失败'.format(file_name))
+    else:
+        pathx = path + str(file_name)
+        open(pathx, 'wb').write(document)
+
+
+path='./'
+url_x="https://github.com/houzi-/CDN/raw/master/"
+
+download_file(url_x,'koolproxy.txt')
+download_file(url_x,'kp.dat')
+download_file(url_x,'daily.txt')
